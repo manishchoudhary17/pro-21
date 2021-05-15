871d059d1a2249ea762982e8aebd71704b0b517f
manishchoudhary17 <82105441+manishchoudhary17@users.noreply.github.com>
2021-05-15 20:52:40 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -21,46 +21,39 @@ function draw(){
 
   background(0);
 
+  
+
+  if (hasCollided(bullet, wall))
+  {
+    bullet.velocityX=0;
+    var damage=0.5 * weight * speed* speed/(thickness *thickness *thickness);
+  
+    
+    if(damage>10)
+    {
+        wall.shapeColor=color(255,0,0);
+    }
+  
+    if(damage<10)
+    {
+      wall.shapeColor=color(0,255,0);
+    }
+  
+  }
 
 
-
-
-
+drawSprites();
 }
 
-function hasCollided(lbullet,lwall);
+function hasCollided(lbullet,lwall)
 {
 
 bulletRightEdge=lbullet.x +lbullet.width;
 wallLeftEdge=lwall.x;
 if (bulletRightEdge>=wallLeftEdge)
 {
-    return true
+    return true;
 }
   return false;
 }
-
-if (hasCollided(bullet, wall))
-{
-  bullet.velocityX=0;
-  var damage=0.5 * weight * speed* speed/(thickness *thickness *thickness);
-
-  |
-  if(damage>10)
-  {
-      wall.shapeColor=color(255,0,0);
-  }
-
-  if(damage<10)
-  {
-    wall.shapeColor=color(0,255,0);
-  }
-
-
-
-
-
-
-}
-
 
